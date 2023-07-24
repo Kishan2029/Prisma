ed5c506df0d5700d864f15afd48240c88468533e
kishan-2029 <kishan.prajapati@searce.com>
2023-07-24 21:42:13 +0530

    added some query

---
+++ prisma/migrations/20230724134345_first_migration/migration.sql
@@ -0,0 +1,82 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('BASIC', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'BASIC',
+    "userPreferanceId" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPreferance" (
+    "id" TEXT NOT NULL,
+    "emailUpdates" BOOLEAN NOT NULL,
+
+    CONSTRAINT "UserPreferance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "averageRating" DOUBLE PRECISION NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "authorId" TEXT NOT NULL,
+    "favouritedById" TEXT,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPost" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_userPreferanceId_key" ON "User"("userPreferanceId");
+
+-- CreateIndex
+CREATE INDEX "User_email_idx" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_age_name_key" ON "User"("age", "name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_userPreferanceId_fkey" FOREIGN KEY ("userPreferanceId") REFERENCES "UserPreferance"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_favouritedById_fkey" FOREIGN KEY ("favouritedById") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_B_fkey" FOREIGN KEY ("B") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -2,8 +2,51 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.findMany();
+  //   await prisma.user.deleteMany();
+
+  const user = await prisma.user.findFirst({
+    where: {
+      name: "Kevin",
+    },
+  });
   console.log("user", user);
+  //Find Unique
+  //   const user = await prisma.user.findUnique({
+  //     where: {
+  //       age_name: {
+  //         age: 22,
+  //         name: "Kevin",
+  //       },
+  //     },
+  //   });
+
+  // Create a multiple user
+  // const user = await prisma.user.createMany({
+  //     data: [
+  //       {
+  //         name: "Kevin",
+  //         age: 14,
+  //         email: "kevin@gmail.com",
+  //       },
+  //       {
+  //         name: "Don",
+  //         age: 25,
+  //         email: "don@gmail.com",
+  //       },
+  //     ],
+  //   });
+  // Creat a single user
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: "Kevin",
+  //       age: 14,
+  //       email: "kevin@gmail.com",
+  //       userPreferance: { create: { emailUpdates: true } },
+  //     },
+  //     include: {
+  //       userPreferance: true,
+  //     },
+  //   });
 }
 
 main()
